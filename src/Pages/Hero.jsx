a6c7a0d815511bee66c891b305bee83f85dcb5b6
mohdArif01 <mohdarif.14011@gmail.com>
2024-06-10 22:10:20 +0530

    New Description

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import "../Pages/Hero.css";
 import Nav from "../Components/Nav";
@@ -9,22 +9,27 @@ const Hero = () => {
   return (
     <>
       <ParticlesContainer />
-      <Nav/>
+      <Nav />
       <div className="retro-grid-container container">
-          <div className="mydet container d-flex flex-column align-items-center justify-content-center">
-            <p className="btn-shine brief">Front-End Developer | UI/UX Designer</p>
-            <h1 className="retro-grid-text">MOHD ARIF</h1>
-            <p className="retro-grid-paragraph ">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem,
-              impedit cupiditate repellendus beatae harum quo! Necessitatibus
-              sequi dolores at doloribus ducimus incidunt et nam!
-            </p>
-            <Link to={'/contact'}>
-              <button type="button" className="btn button"> <span className="letstalk">Let's Talk</span>  <span className="itsfree" >- it's free</span> </button>
-            </Link>
-            
-          </div>
-        
+        <div className="mydet container d-flex flex-column align-items-center justify-content-center">
+          <p className="btn-shine brief">
+            Front-End Developer | UI/UX Designer
+          </p>
+          <h1 className="retro-grid-text">MOHD ARIF</h1>
+          <p className="retro-grid-paragraph ">
+            Transforming ideas into stunning visuals and intuitive experiences.
+            As a UI/UX and graphic designer, I create eye-catching banners and
+            t-shirts. As a frontend developer, I bring those designs to life.
+            Let's design, code, and inspire together!
+          </p>
+          <Link to={"/contact"}>
+            <button type="button" className="btn button">
+              {" "}
+              <span className="letstalk">Let's Talk</span>{" "}
+              <span className="itsfree">- it's free</span>{" "}
+            </button>
+          </Link>
+        </div>
       </div>
     </>
   );
